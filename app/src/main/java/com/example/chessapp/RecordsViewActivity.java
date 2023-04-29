ceae5c01fd0ce564a34c3b2f86fb2dd8c79fa439
William Gitoneas <williamtgit@yahoo.com>
2023-04-29 02:59:36 -0400

    activity

---
+++ app/src/main/java/com/example/chessapp/RecordsViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.chessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordsViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records_view);
+    }
+}
