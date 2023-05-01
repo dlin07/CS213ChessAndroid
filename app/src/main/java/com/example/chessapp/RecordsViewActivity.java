01ac665577e1fb4cc67fdc2df0d910d8c64de4ba
William Gitoneas <williamtgit@yahoo.com>
2023-05-01 17:59:13 -0400

    deleted data folder and code updates

--- app/src/main/java/com/example/chessapp/RecordsViewActivity.java
+++ app/src/main/java/com/example/chessapp/RecordsViewActivity.java
@@ -5,6 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class RecordsViewActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +26,39 @@ public class RecordsViewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String sortType = intent.getStringExtra("sortType");
 
+        Path records = Paths.get("/data/user/0/com.example.chessapp/files/records.txt");
+        try {
+            if(!Files.exists(records)){
+                System.out.println("creating file");
+                Files.createFile(records);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        FileInputStream in;
+        try {
+            in = openFileInput("records.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line;
+        while (true) {
+            try {
+                if (!((line = bufferedReader.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(line);
+        }
+        try {
+            inputStreamReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
