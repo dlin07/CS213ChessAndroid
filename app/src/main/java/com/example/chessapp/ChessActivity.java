d2219935d54c2a416dff8b86a54c14d59cf1f477
dlin07 <74471750+dlin07@users.noreply.github.com>
2023-04-30 22:24:47 -0400

    finished building board

--- app/src/main/java/com/example/chessapp/ChessActivity.java
+++ app/src/main/java/com/example/chessapp/ChessActivity.java
@@ -3,6 +3,7 @@ package com.example.chessapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridLayout;
 
 public class ChessActivity extends AppCompatActivity {
 
@@ -14,5 +15,12 @@ public class ChessActivity extends AppCompatActivity {
 
     public void pieceClicked() {
         //TODO: add implementation for when a piece is clicked
+    }
+
+    public void onDraw() {
+        GridLayout grid = new GridLayout(this);
+
+        // draws the 8x8 chess board
+
     }
 }
