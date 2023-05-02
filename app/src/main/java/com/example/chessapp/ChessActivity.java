98529f100733accb01561b5ca5eb5fb4487ac5f4
William Gitoneas <williamtgit@yahoo.com>
2023-05-01 20:02:32 -0400

    bug fixes with button clicking

--- app/src/main/java/com/example/chessapp/ChessActivity.java
+++ app/src/main/java/com/example/chessapp/ChessActivity.java
@@ -30,6 +30,9 @@ public class ChessActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess);
 
+        // starts recording the game
+        Record record = new Record();
+
         // hashmap containing pieces and their id's
         pieces = new HashMap<>();
         pieces.put("wp", R.drawable.wpawn);
@@ -56,14 +59,14 @@ public class ChessActivity extends AppCompatActivity {
         if (r1 == -1) {
             for (int i = 0; i < 64; i++) {
                 if (boardView.getChildAt(i) == v) {
-                    r1 = 8-(int)(i / 8);
+                    r1 = 8 - (int) (i / 8);
                     c1 = i % 8;
                 }
             }
         } else {
             for (int i = 0; i < 64; i++) {
                 if (boardView.getChildAt(i) == v) {
-                    r2 = 8-(int)(i / 8);
+                    r2 = 8 - (int) (i / 8);
                     c2 = i % 8;
                 }
             }
@@ -76,7 +79,7 @@ public class ChessActivity extends AppCompatActivity {
             // boardView.removeAllViews();
 
             for (int i = 0; i < 64; i++) {
-                int j = i*3;
+                int j = i * 3;
 
                 ImageView image = (ImageView) boardView.getChildAt(i);
                 String test = game.printBoard(game.getBoard());
@@ -100,65 +103,62 @@ public class ChessActivity extends AppCompatActivity {
 
     // draws game when button is clicked
     public void handleDraw(View view) {
-        //TODO: implement actual draw game functionality and see why you need to press button twice
-        Button draw = findViewById(R.id.draw);
-        draw.setOnClickListener(v -> {
-            // popup window appears stating that game is a draw
-            // if ok is clicked, popup window disappears and new activity for saving game appears
+        //TODO: implement actual draw game functionality
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(ChessActivity.this);
-            builder.setMessage(R.string.Draw);
-            // Add the buttons
-            builder.setPositiveButton(R.string.Ok, (dialog, id) -> {
-                // User clicked OK button
-                // new activity for saving game appears
+        // popup window appears stating that game is a draw
+        // if ok is clicked, popup window disappears and new activity for saving game appears
 
-                //Intent intent = new Intent(ChessActivity.this, SaveGameActivity.class);
-                //startActivity(intent);
-            });
-            builder.setNegativeButton(R.string.Cancel, (dialog, id) -> {
-                // User cancelled the dialog
-                // goes back to main activity
+        AlertDialog.Builder builder = new AlertDialog.Builder(ChessActivity.this);
+        builder.setMessage(R.string.Draw);
+        // Add the buttons
+        builder.setPositiveButton(R.string.Ok, (dialog, id) -> {
+            // User clicked OK button
+            // new activity for saving game appears
 
-                Intent intent = new Intent(ChessActivity.this, MainActivity.class);
-                startActivity(intent);
-            });
-
-            // Create the AlertDialog
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            //Intent intent = new Intent(ChessActivity.this, SaveGameActivity.class);
+            //startActivity(intent);
         });
+        builder.setNegativeButton(R.string.Cancel, (dialog, id) -> {
+            // User cancelled the dialog
+            // goes back to main activity
+
+            Intent intent = new Intent(ChessActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     // resigns game when button is clicked
     public void handleResign(View view) {
         //TODO: implement actual resign game functionality
-        Button resign = findViewById(R.id.resign);
-        resign.setOnClickListener(v -> {
-            // popup window appears stating that game is a resign
-            // if ok is clicked, popup window disappears and new activity for saving game appears
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(ChessActivity.this);
-            // builder.setMessage(R.string.Resign);
-            // Add the buttons
-            builder.setPositiveButton(R.string.Ok, (dialog, id) -> {
-                // User clicked OK button
-                // new activity for saving game appears
+        // popup window appears stating that game is a resign
+        // if ok is clicked, popup window disappears and new activity for saving game appears
 
-                //Intent intent = new Intent(ChessActivity.this, SaveGameActivity.class);
-                //startActivity(intent);
-            });
-            builder.setNegativeButton(R.string.Cancel, (dialog, id) -> {
-                // User cancelled the dialog
-                // goes back to main activity
+        AlertDialog.Builder builder = new AlertDialog.Builder(ChessActivity.this);
+        // builder.setMessage(R.string.Resign);
+        // Add the buttons
+        builder.setPositiveButton(R.string.Ok, (dialog, id) -> {
+            // User clicked OK button
+            // new activity for saving game appears
 
-                Intent intent = new Intent(ChessActivity.this, MainActivity.class);
-                startActivity(intent);
-            });
-
-            // Create the AlertDialog
-            AlertDialog dialog = builder.create();
-            dialog.show();
+            //Intent intent = new Intent(ChessActivity.this, SaveGameActivity.class);
+            //startActivity(intent);
         });
+        builder.setNegativeButton(R.string.Cancel, (dialog, id) -> {
+            // User cancelled the dialog
+            // goes back to main activity
+
+            Intent intent = new Intent(ChessActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
